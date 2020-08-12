706d03a052992b754e7a46d84138ad99eada2073
Ghezin <67123872+Ghezin@users.noreply.github.com>
2020-08-11 21:52:17 -0600

    Update Main.php

--- src/ghezin/main/Main.php
+++ src/ghezin/main/Main.php
@@ -6,27 +6,16 @@ namespace ghezin\main;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\utils\Config;
-use pocketmine\network\mcpe\protocol\EventPacket;
-use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use ghezin\main\listeners\EventListener;
-use ghezin\main\FormUtils;
 
 class Main extends PluginBase{
-	
-	private static $instance;
 	
 	private $availableCtrls=["Unknown", "Mouse", "Touch", "Controller"];
 	
 	public $controls=[];
 	
 	public function onEnable():void{
-		self::$instance=$this;
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-	}
-	public static function getInstance():Core{
-		return self::$instance;
 	}
 	public function getPlayerControls($player):?string{
 		if($player instanceof Player) $player=$player->getName();
